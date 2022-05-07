75764e832d9282677a0f6955d6f0e975a620d030
Ketan Singh <ketan.singh_cs19@gla.ac.in>
2022-05-07 21:02:29 +0530

    Initial commit

---
+++ My_Recipies.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My recipe</title>
+    <link rel="stylesheet" href="./recipies_style.css">
+</head>
+<body>
+<section class="menu">
+    <a href="./Foodie_At_Home.html">Home</a>
+    <a href="first.php">About us</a>
+</section>
+<div class="recipe">
+    <div class="recipe-img">
+        <img src="Aloo-Paratha.webp" alt="recipe-img">
+    </div>
+    <div class="recipe-content">
+        <h3>Aloo Paratha</h3>
+        <p>First prepare dough by taking 1 cup wheat flour, 1 teaspoon oil and salt in a small bowl. Incrementally add water in small quantities and knead smooth and soft dough (like chapati dough). Grease its surface with 1/2 teaspoon oil, cover it with clean muslin cloth and set aside for 15-20 minutes to settle at room temperature.<br>
+            step-1 <br>
+            Now, prepare alu paratha stuffing. Take the boiled potatoes in a medium bowl and mash it until no chunks of potato. Add chopped green chillies, red chilli powder, grated ginger, garam masala powder, sugar, lemon juice and salt. Mix all ingredients properly. <br>
+            step-2 <br>
+            Divide stuffing into 6 equal parts and give them a shape of ball. Divide prepared dough into 6 equal parts, give them a round shape and press a little to flatten like pattie. <br>
+            step-3 <br>
+            Take 1/2 cup dry wheat flour in a small plate for dusting. Take one dough pattie and coat it with dry wheat flour on both sides. (It will prevent dough from sticking to rolling board). Put it on rolling board/roti making board (chakla) and roll it out into a circle of approx. 4-5 inch diameter using rolling pin. Put one portion of stuffing in the center of it. <br>
+            step-4 <br>
+            Wrap stuffing ball by lifting all sides of rolled circle towards top, seal the edges and again give it a round ball shape. <br>
+        </p>
+    </div>
+</div>
+
+<div class="recipe">
+    <div class="recipe-img">
+        <img src="chow-mein.jpg" alt="recipe-img">
+    </div>
+    <div class="recipe-content">
+        <h3>Chow - Mein </h3>
+        <p>Step 1 <br>
+            Cook 225g egg noodles in a large pan of boiling water for 3-5 mins, then drain and put them in cold water. Drain thoroughly, toss them with 1 tbsp sesame oil and set aside. <br>
+            Step 2 <br>
+            Combine 100g chicken breasts, cut into fine shreds, with 2 tsp light soy sauce, 2 tsp Shaohsing rice wine or dry sherry, 1 tsp sesame oil, ½ tsp white pepper and ½ tsp salt for the marinade, mix well and then leave to marinate for about 10 mins.
+            <br>
+            Step 3 <br>
+            Heat a wok over a high heat. Add 1 tbsp groundnut oil and, when it is very hot and slightly smoking, add the chicken shreds.
+            <br>
+            Step 4 <br>
+            Stir-fry for about 2 mins and then transfer to a plate.
+            <br>
+            Step 5 <br>
+            Wipe the wok clean, reheat until it is very hot then add 1½ tbsp groundnut oil.
+            <br>
+            Step 6 <br>
+            When the oil is slightly smoking, add the 2 finely chopped garlic cloves and stir-fry for 10 seconds.
+            <br>
+            Step 7 <br>
+            Add 50g finely shredded mangetout and 50g finely shredded prosciutto, and stir-fry for about 1 min.
+            <br>
+            Step 8 <br>
+            Add the noodles, 2 tsp light soy sauce, 2 tsp dark soy sauce,1 tbsp Shaohsing rice wine or dry sherry, ½ tsp white pepper, ½ tsp golden caster sugar, 2 finely chopped spring onions and 1 tsp salt.
+            <br>
+            Step 9 <br>
+            Stir-fry for 2 mins. Return the chicken and any juices to the noodle mixture. Stir-fry for about 3-4 mins or until the chicken is cooked.
+            <br>
+            Step 10 <br>
+            Add 1 tsp sesame oil and give the mixture a few final stirs. Put on a warm platter and serve immediately.
+        </p>
+    </div>
+</div>
+</body>
+</html>
---
+++ first.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyWebSite</title>
+    <link rel="stylesheet" href="about_style.css">
+</head>
+
+<body>
+
+    <header>
+        <a href="#" class="brand">KRANGBAX!</a>
+        <div class="menu-btn"></div>
+        <div class="navbar">
+            <a class="active" href="./Foodie_At_Home.html">Home</a>
+            <a href="../Ketan%20Singh's%20Resume.pdf">Resume</a><br>
+          </div>
+    </header>
+    <section class="main" id="main">
+        <div class="content"><br>
+            <h2>Hello, I am<br><span>Ketan Singh</span></h2>
+            <div class="animated-text">
+                <h3>A Full Stack  Developer</h3>
+                <h3>Programmer</h3>
+            </div>
+        </div>
+
+    </section>
+    <section class="about" id="about">
+        <div class="title">
+            <h2 class="section-title">About Me</h2>
+        </div>
+        <div class="content">
+            <div class="column col-left">
+                <div class="img-card">
+                    <img src="./my_image.jpeg" alt="">
+                </div>
+
+            </div>
+            <div class="column col-right ">
+                <h2 class="content-title">Hii Guys<br>
+                     I am Ketan Singh</h2><br>
+                <p class="paragraph-text">I am an Information Technology Student and i have Knowledge About Data Structure and Algorithm and machine Learning <br>For this time I have Completed all types of Structure. Such as LinkedList , Circular LinkedList , Binary Tree ,AVL Tree , and Searching and sorting Algorithm <br>I am Always happy to learn and developing their professional skills and learning new things all the time.<br>
+                    <br>Currently I am going to as a Web Developer. If you Interested Then Contact with me </p>
+               
+            </div>
+        </div>
+    </section>
+    <section class="services" id="services">
+        <div class="title ">
+                   <div class="content">
+            <div class="card ">
+                <div class="services-icons">
+                    <i class="fab fa-html5"></i>
+                </div>
+                <div class="info">
+                    <h3>HTML</h3>
+                    <p>Publish online documents with headings, text, tables, lists, photos, etc.
+                        Retrieve online information via hypertext links, at the click of a button.
+                        Design forms for conducting transactions with remote services, for use in searching for information, making reservations, ordering products, etc.
+                        Include spread-sheets, video clips, sound clips, and other applications directly in their documents.</p>
+                </div>
+            </div>
+            <div class="card ">
+                <div class="services-icons">
+                    <i class="fab fa-css3-alt"></i>
+                </div>
+                <div class="info">
+                    <h3>CSS</h3>
+                    <p>CSS is the language for describing the presentation of Web pages, including colors, layout, and fonts.
+                        It allows one to adapt the presentation to different types of devices, such as large screens, small screens, or printers.
+                        CSS is independent of HTML and can be used with any XML-based markup language. The separation of HTML from CSS makes it easier to maintain sites, share style sheets across pages, and tailor pages to different environments.
+                        This is referred to as the separation of structure (or: content) from presentation.</p>
+                </div>
+            </div>
+            <div class="card ">
+                <div class="services-icons">
+                    <i class="fab fa-js"></i>
+                </div>
+                <div class="info">
+                    <h3>PHP</h3>
+                    <p>PHP is a lightweight, interpreted programming language.
+                        Designed for creating network-centric applications.
+                        Complementary to and integrated with HTML.
+                        Open and cross-platform
+                        Less server interaction
+                        Immediate feedback to the visitors
+                        Increased interactivity
+                        Richer interfaces</p>
+                </div>
+            </div>
+
+    </section>
+
+
+    <!-- price table section ends -->
+        <section class="contact" id="contact">
+        <div class="title ">
+            <h2 class="section-title">Contact Me</h2>
+        </div>
+        <div class="content">
+            <div class="row">
+                <div class="card ">
+                    <div class="contact-icon">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="info">
+                        <a href="https://www.instagram.com/krangbax/" target="=_blank" class="d_inline">Instagram</a>
+                    </div>
+                </div>
+                <div class="card ">
+                    <div class="contact-icon">
+                        <i class="fab fa-linkedin-in"></i>
+                    </div>
+                    <div class="info">
+                        <a href="https://www.linkedin.com/in/ketan-singh-b7738a167/" target="_blank"
+                  class="d-inline">LinkedIn</a>
+                    </div>
+                </div>
+                <div class="card ">
+                    <div class="contact-icon">
+                        <i class="fab fa-github"></i>
+                    </div>
+                    <div class="info">
+                        <a href="https://github.com/Ketansingh07" target="_blank" class="d-inline">GitHub</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="contact-form ">
+                    <h3>Send Message</h3>
+                    <h4>Hii,How can i help You?<br>
+                        Please Send Me Some Details
+                    </h4>
+                    <div class="input-box">
+                        <input type="text" name="" value="" placeholder="Name">
+                    </div>
+                    <div class="input-box">
+                        <input type="text" name="" value="" placeholder="Email">
+                    </div>
+                    <div class="input-box">
+                        <input type="text" name="" value="" placeholder="Contact Number">
+                    </div>
+                    <div class="input-box">
+                        <textarea name="name" rows="8" cols="70" placeholder="Message"></textarea>
+                    </div>
+
+                    <div class="input-box">
+                        <input type="submit" class="send-btn" name="" value="Send">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
